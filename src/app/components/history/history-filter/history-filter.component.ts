6b6c110a1a12f814a4e108d143a39ed399d8f54b
haruki-isoda-Pathos <haruki.isoda@pathoslogos.co.jp>
2026-05-15 16:41:54 +0900

    susurutv

--- src/app/components/history/history-filter/history-filter.component.ts
+++ src/app/components/history/history-filter/history-filter.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core'
-import { HistoryComponent } from '../history.component'
+import { Component, Output, EventEmitter } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component ({
     selector: 'app-history-filter',
     standalone: true,
-    imports: [HistoryComponent],
+    imports: [CommonModule, FormsModule],
     templateUrl: './history-filter.component.html',
     styleUrls:['./history-filter.component.css'],
 })
 
-export class HistoryFilterComponent {}
+export class HistoryFilterComponent {
+
+filterOption: string | null = null;
+
+@Output() filterChanged = new EventEmitter<string | null>();
+
+onFilterChange() {
+    this.filterChanged.emit(this.filterOption);
+  }
+
+}
--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HistoryService, History } from '../../services/history.service';
+import { HistoryFilterComponent } from './history-filter/history-filter.component'
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports:[CommonModule],
+  imports:[CommonModule, HistoryFilterComponent],
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
 
 export class HistoryComponent implements OnInit{
 
-histories: History[] = []
+histories: History[] = [];
+filteredHistories: History[] = [];
+currentFilter: string | null = null;
 showClearConfirm: boolean = false;
 
 constructor(
@@ -22,9 +25,26 @@ constructor(
 
 ngOnInit() {
       this.historyService.histories$.subscribe(histories => {
-        this.histories = histories
+        this.histories = histories;
         this.cdr.detectChanges()
+        this.applyFilter();
       });
+}
+
+onFilterChanged(action: string | null) {
+  this.currentFilter = action;
+  this.applyFilter();
+}
+
+applyFilter() {
+  if (!this.currentFilter) {
+    this.filteredHistories = this.histories;
+    return;
+  }
+  this.filteredHistories =
+    this.histories.filter(
+      history => history.action === this.currentFilter
+    );
 }
 
 confirm() {
